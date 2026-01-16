20c25efc47dba6808cf98a87c1d34e1eacb96e5c
Parishi <parishithada@gmail.com>
2026-01-16 13:31:48 +0530

    Initial commit

---
+++ rate-limiter/src/test/java/com/systemdesign/ratelimiter/RatelimiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systemdesign.ratelimiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatelimiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
